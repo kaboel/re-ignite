bd18a0b678f5d53234b7050ed77df645b1f8e8ce
Faiq Allam <faiq.kaboel@gmail.com>
2020-04-16 16:38:01 +0700

    add  database creds

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: "AIzaSyDij37lXokL2ovuycpBFHLcufgNTMh4e0E",
+  authDomain: "react-first-f6eb0.firebaseapp.com",
+  databaseURL: "https://react-first-f6eb0.firebaseio.com",
+  projectId: "react-first-f6eb0",
+  storageBucket: "react-first-f6eb0.appspot.com",
+  messagingSenderId: "196148200462",
+  appId: "1:196148200462:web:bfb0a02ffd25315398e85f",
+  measurementId: "G-JLF1RFEBZC",
+};
